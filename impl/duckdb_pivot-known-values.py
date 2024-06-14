a6da7abc7309cf0a8ec19b97c8d3aebf347f4e07
jsj <jsj>
2024-06-14 21:19:13 +0200

    added duckdb with known values

---
+++ impl/duckdb_pivot-known-values.py
@@ -0,0 +1,138 @@
+import shutil
+import sys
+from pathlib import Path
+
+import duckdb
+from data_generation.helpers import BenchmarkWriter, DatasetSizes
+from rich import print
+
+# See lib.rs for details about constants
+CARD_TYPES = ("DC", "CC")
+TRANSACTION_TYPES = (
+    "food-and-household",
+    "home",
+    "uncategorized",
+    "leisure-and-lifestyle",
+    "health-and-beauty",
+    "shopping-and-services",
+    "children",
+    "vacation-and-travel",
+    "education",
+    "insurance",
+    "investments-and-savings",
+    "expenses-and-other",
+    "cars-and-transportation",
+)
+CHANNELS = ("mobile", "web")
+
+
+# Required time windows
+WINDOWS_IN_DAYS = (
+    7,  # week
+    14,  # two weeks
+    21,  # three weeks
+    30,  # month
+    90,  # three months
+    180,  # half of the year
+    360,  # year
+    720,  # two years
+)
+
+
+# Information for result
+NAME = "DuckDB pivot"
+
+
+if __name__ == "__main__":
+    path = sys.argv[1]
+
+    if "tiny" in path:
+        task_size = DatasetSizes.TINY
+    elif "small" in path:
+        task_size = DatasetSizes.SMALL
+    elif "medium" in path:
+        task_size = DatasetSizes.MEDIUM
+    else:
+        task_size = DatasetSizes.BIG
+
+    shutil.rmtree("tmp_out", ignore_errors=True)
+    shutil.rmtree("tmp_spill", ignore_errors=True)
+
+    results_prefix = Path(__file__).parent.parent.joinpath("results")
+    helper = BenchmarkWriter(NAME, task_size, results_prefix)
+
+    #  #   Column       Dtype
+    # ---  ------       -----
+    #  0   customer_id  int64
+    #  1   card_type    object
+    #  2   trx_type     object
+    #  3   channel      object
+    #  4   trx_amnt     float64
+    #  5   t_minus      int64
+    #  6   part_col     category
+
+    card_type_values = "','".join(CARD_TYPES)
+    trx_type_values = "','".join(TRANSACTION_TYPES)
+    channel_values = "','".join(CHANNELS)
+    win_values = ",".join(str(win) for win in WINDOWS_IN_DAYS)
+
+    sql = f"""
+        with base_table as (
+            select
+                *
+            from
+                read_parquet("./{path}/**/*.parquet")
+            ),
+        all_windows as (
+            select 
+                *,
+                case 
+                    {
+                        " ".join([
+                            f" when t_minus <= {win} then {win} " for win in WINDOWS_IN_DAYS
+                        ])
+                    }
+                end as t_minus_1
+            from base_table
+        ),
+        card_type_agg as (
+            pivot all_windows
+            on 
+                card_type in ('{card_type_values}'),
+                trx_type in ('{trx_type_values}'),
+                t_minus_1 in ({win_values})
+            using count(trx_amnt) as 'd_count', mean(trx_amnt) as 'd_mean', sum(trx_amnt) as 'd_sum', min(trx_amnt) as 'd_min', max(trx_amnt) as 'd_max'
+            group by customer_id
+        ),
+        channel_type_agg as ( 
+            pivot all_windows
+            on 
+                channel in ('{channel_values}'),
+                trx_type in ('{trx_type_values}'),
+                t_minus_1 in ({win_values})
+            using count(trx_amnt) as 'd_count', mean(trx_amnt) as 'd_mean', sum(trx_amnt) as 'd_sum', min(trx_amnt) as 'd_min', max(trx_amnt) as 'd_max'
+            group by customer_id 
+        )
+        select
+            a.*,
+            b.* exclude(customer_id)
+        from card_type_agg a
+        full join channel_type_agg b
+            on a.customer_id = b.customer_id
+    """
+
+    sql = f"""
+        set temp_directory = '../tmp_spill/';
+        copy (
+            {sql}
+        ) to '../tmp_out'
+        (format 'parquet', compression 'zstd');
+    """
+    # Start the work
+    helper.before()
+
+    duckdb.connect(":memory:").execute(sql)
+
+    total_time = helper.after()
+    print(f"[italic green]Total time: {total_time} seconds[/italic green]")
+    sys.exit(0)
